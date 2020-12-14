d66a7a24cf1941060553c3458243dfdf5ea8f478
Nidvar <djayshine87@hotmail.com>
2020-12-14 22:40:50 +1100

    added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-
 import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamDelete from './streams/StreamDelete';
+import StreamShow from './streams/StreamShow';
 
 function App() {
   return (
     <div className="App">
-      <h1>Front Side</h1>
+      <BrowserRouter>
+        <h1>Front Side</h1>
+        <Route exact path ='/' component = {StreamList}/>
+        <Route path ='/streams/new' component = {StreamCreate}/>
+        <Route path ='/streams/edit' component = {StreamEdit}/>
+        <Route path ='/streams/delete' component = {StreamDelete}/>
+        <Route path ='/streams/show' component = {StreamShow}/>
+      </BrowserRouter>
     </div>
   );
 }
